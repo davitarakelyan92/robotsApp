48f9425e34600d1b30126e9d6e9a3bf7983f002f
Davit Arakelyan <davitarakelyan92@gmail.com>
2019-03-07 16:38:00 +0400

    New Project

---
+++ precache-manifest.62e2dc4d99698ce1616e3aaaa941736c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "554dd574b1cfaf000c45",
+    "url": "/static/js/main.554dd574.chunk.js"
+  },
+  {
+    "revision": "95985d9aa384512247d8",
+    "url": "/static/js/2.95985d9a.chunk.js"
+  },
+  {
+    "revision": "554dd574b1cfaf000c45",
+    "url": "/static/css/main.8e9754ba.chunk.css"
+  },
+  {
+    "revision": "95985d9aa384512247d8",
+    "url": "/static/css/2.766021ff.chunk.css"
+  },
+  {
+    "revision": "8dab954d3a6d27987278d4c9107756a6",
+    "url": "/index.html"
+  }
+];
